a35bbfee9536e9aa21c37d78daedc7cadbed1f3c
shkawan <shingo.ms@55mp.com>
2018-08-18 17:52:47 +0900

    modify

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -6,7 +6,7 @@ class TestHandlerCase(unittest.TestCase):
 
     def test_response(self):
         print("testing response.")
-        result = index.handler('{"body":"test"}', None)
+        result = index.handler({"body":"test"}, None)
         print(result)
         self.assertEqual(result['statusCode'], 200)
         self.assertEqual(result['headers']['Content-Type'], 'application/json')
