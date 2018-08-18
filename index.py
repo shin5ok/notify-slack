61688c71375edfcadb87821d9c12348dc4a1fc92
shkawan <shingo.ms@55mp.com>
2018-08-18 15:18:43 +0900

    fix

--- index.py
+++ index.py
@@ -2,10 +2,12 @@ import json
 import datetime
 import requests
 
-url = "http://uname.link/slack/kawano"
+url = "http://t2.uname.link/slack/kawano"
 def handler(event, context):
     data = event
-    requests.post(url, data=data)
+    print(data)
+    r = requests.post(url, data=data)
+    print(r.content)
 
     return {'statusCode': 200,
             'body': json.dumps(data),
