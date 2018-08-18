83c735521d05f01930e3cfc608d4723a674ac704
shkawan <shingo.ms@55mp.com>
2018-08-18 14:53:53 +0900

    Initial Commit for testing

--- index.py
+++ index.py
@@ -1,12 +1,11 @@
 import json
 import datetime
-
+import requests
 
 def handler(event, context):
-    data = {
-        'output': 'Hello World',
-        'timestamp': datetime.datetime.utcnow().isoformat()
-    }
+    data = event
+    requests.post(url, data=data)
+
     return {'statusCode': 200,
             'body': json.dumps(data),
             'headers': {'Content-Type': 'application/json'}}
