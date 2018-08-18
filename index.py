a254a26dee2c92b1e9d225c734d32e7164ee5a57
shkawan <shingo.ms@55mp.com>
2018-08-18 17:03:43 +0900

    modify

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import requests
 
 url = "http://t2.uname.link/slack/kawano"
 def handler(event, context):
-    data = event['body']
+    data = json.loads(event)['body']
     print(data)
     r = requests.post(url, data=data)
     print(r.content)
